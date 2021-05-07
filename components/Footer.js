70e0d569c38c6b2e2933bb03cd59abd815b39f39
Jesse Chen <jessechen@fb.com>
2021-05-06 22:33:18 -1000

    Change opacity on hover for footer links

--- components/Footer.js
+++ components/Footer.js
@@ -3,18 +3,30 @@ import { FiLinkedin, FiGithub, FiTwitter, FiMail } from "react-icons/fi"
 
 export default function Footer() {
   return (
-    <VStack py="12" spacing="4">
-      <HStack spacing="4">
-        <Link href="https://www.linkedin.com/in/0xjessel" isExternal>
+    <VStack py="12" spacing="2">
+      <HStack spacing="1">
+        <Link 
+          padding="2"
+          _hover={{ opacity: .7 }}
+          href="https://www.linkedin.com/in/0xjessel" isExternal>
           <FiLinkedin />
         </Link>
-        <Link href="https://github.com/0xjessel" isExternal>
+        <Link 
+          padding="2"
+          _hover={{ opacity: .7 }}
+          href="https://github.com/0xjessel" isExternal>
           <FiGithub />
         </Link>
-        <Link href="https://twitter.com/0xjessel" isExternal>
+        <Link 
+          padding="2"
+          _hover={{ opacity: .7 }}
+          href="https://twitter.com/0xjessel" isExternal>
           <FiTwitter />
         </Link>
-        <Link href="mailto:contact@jessechen.net" isExternal>
+        <Link 
+          padding="2"
+          _hover={{ opacity: .7 }}
+          href="mailto:contact@jessechen.net" isExternal>
           <FiMail />
         </Link>
       </HStack>
