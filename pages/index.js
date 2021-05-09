9b6d144ff7f4cd46472c57827331ce5f0720a237
Jesse Chen <jessechen@fb.com>
2021-05-08 22:23:16 -1000

    Delete unused references in index.js
    Oops.

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,9 @@
-import fs from 'fs'
-import matter from 'gray-matter'
-import Link from 'next/link'
-import path from 'path'
 import Layout from '../components/Layout'
-import { postFilePaths, POSTS_PATH } from '../utils/mdxUtils'
 import { Box, Container, Divider, Grid, GridItem, Heading, Image, Text } from '@chakra-ui/react'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 
-export default function Index({ posts }) {
+export default function Index() {
   return (
     <Layout>
       <Header />
@@ -51,19 +46,4 @@ export default function Index({ posts }) {
       <Footer />
     </Layout>
   )
-}
-
-export function getStaticProps() {
-  const posts = postFilePaths.map((filePath) => {
-    const source = fs.readFileSync(path.join(POSTS_PATH, filePath))
-    const { content, data } = matter(source)
-
-    return {
-      content,
-      data,
-      filePath,
-    }
-  })
-
-  return { props: { posts } }
 }
