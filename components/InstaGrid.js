d2b0c33525925bfe6860d7b0d9ec191a73c033f8
Jesse Chen <jessechen@fb.com>
2021-05-14 00:07:20 -1000

    Cleanup isVideo check in InstaGrid

--- components/InstaGrid.js
+++ components/InstaGrid.js
@@ -12,36 +12,37 @@ export default function InstaGrid({ medias, ...props }) {
       {...props}
       columns={[2, 3]}
       spacing="4">
-      {medias.map((media) => 
-        <LinkOverlay
-          isExternal
-          href={media.permalink}
-          key={media.id}
-          css={media.media_type === "VIDEO" 
-            ? {
-                position: 'relative',
-                '& svg' : {
-                  position: 'absolute',
-                  top: '50%',
-                  left: '50%',
-                  transform: 'translate(-50%, -50%)',
+      {medias.map((media) => {
+        const isVideo = media.media_type === "VIDEO" 
+
+        return (
+          <LinkOverlay
+            isExternal
+            href={media.permalink}
+            key={media.id}
+            css={isVideo
+              ? {
+                  position: 'relative',
+                  '& svg' : {
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                  }
+                } : {
+                  position: 'relative',
                 }
-              } : {
-                position: 'relative',
-              }
-          }>
-          <Image
-            width="100%"
-            css={{ 'aspect-ratio': '1' }}
-            objectFit="cover"
-            src={media.media_type === "VIDEO" ? media.thumbnail_url : media.media_url}
-            alt="Instagram photo"
-          />
-          {media.media_type === "VIDEO"
-            ? <Icon as={FaPlay} boxSize="8" />
-            : null
-          }
-        </LinkOverlay>
+            }>
+            <Image
+              width="100%"
+              css={{ 'aspect-ratio': '1' }}
+              objectFit="cover"
+              src={isVideo ? media.thumbnail_url : media.media_url}
+              alt="Instagram photo"
+            />
+            {isVideo ? <Icon as={FaPlay} boxSize="8" /> : null}
+          </LinkOverlay>
+        )}
       )}
     </SimpleGrid>
   )
