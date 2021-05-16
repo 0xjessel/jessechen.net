d0b66667507c182031a2dd2964266edadd288b58
Jesse Chen <jessechen@fb.com>
2021-05-16 02:43:58 -1000

    `AspectRatio` is the way to go

--- components/InstaGrid.js
+++ components/InstaGrid.js
@@ -1,4 +1,4 @@
-import { Box, LinkOverlay, SimpleGrid } from "@chakra-ui/layout";
+import { AspectRatio, Box, LinkOverlay, SimpleGrid } from "@chakra-ui/layout";
 import { Icon } from "@chakra-ui/react";
 import { FaPlay } from "react-icons/fa"
 import NImage from "next/image"
@@ -16,42 +16,32 @@ export default function InstaGrid({ medias, ...props }) {
       {medias.map((media) => {
         const isVideo = media.media_type === "VIDEO" 
 
-        const boxBaseCSS = {
-          position: 'relative',
-          '&::before': {
-            content: '""',
-            display: "block",
-            "paddingBottom": "100%"
-          },
-        }
-
         return (
           <LinkOverlay
             isExternal
             href={media.permalink}
             key={media.id}
-            position="relative">
-            <Box
-              css={isVideo
-                ? {
-                    ...boxBaseCSS,
-                    '& svg' : {
-                      position: 'absolute',
-                      top: '50%',
-                      left: '50%',
-                      transform: 'translate(-50%, -50%)',
-                    }
-                  } 
-                : boxBaseCSS
-                }>
+            css={isVideo
+              ? {
+                  position: "relative",
+                  '& svg' : {
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                  }
+                } 
+              : {}
+            }>
+            <AspectRatio ratio="1">
               <NImage
                 layout="fill" 
                 objectFit="cover"
                 src={isVideo ? media.thumbnail_url : media.media_url}
                 alt="Instagram photo"
               />
-              {isVideo ? <Icon as={FaPlay} boxSize="8" /> : null}
-            </Box>
+            </AspectRatio>
+            {isVideo ? <Icon as={FaPlay} boxSize="6" /> : null}
           </LinkOverlay>
         )}
       )}
