a9cb1c16367d65282ab15cd309d89a1db95cbb4b
Jesse Chen <jessechen@fb.com>
2021-05-22 23:48:57 -1000

    Add more IG subdomains

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   images: {
-    domains: ['scontent.cdninstagram.com', 'scontent-iad3-2.cdninstagram.com'],
+    domains: [
+      'scontent.cdninstagram.com', 
+      'scontent-iad3-2.cdninstagram.com', 
+      'scontent-lax3-1.cdninstagram.com',
+      'scontent-lax3-2.cdninstagram.com',
+    ],
   },
 }
