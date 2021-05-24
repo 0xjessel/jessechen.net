7324869a0feabd62324a1c0638e1a94df18429eb
Jesse Chen <jessechen@fb.com>
2021-05-23 17:33:05 -1000

    Support gold-themed text selection

--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,17 @@ import { Container } from "@chakra-ui/react"
 export default function Layout({ children }) {
   return (
     <>
-      <Container as="main" maxW="2xl" px={4} py={8}>
+      <Container 
+        as="main" 
+        maxW="2xl" 
+        px={4} 
+        py={8}
+        css={{
+          '::selection': {
+            color: 'blue',
+            background: 'tomato',
+          },
+        }}>
         {children}
       </Container>
     </>
--- styles/app-theme.js
+++ styles/app-theme.js
@@ -1,4 +1,5 @@
-import { extendTheme } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react"
+import { mode } from "@chakra-ui/theme-tools"
 
 export const AppTheme = extendTheme({
   config: {
@@ -11,7 +12,7 @@ export const AppTheme = extendTheme({
     mono: "Consolas, Menlo, Monaco, source-code-pro, Courier New, monospace",
   },
   styles: {
-    global: {
+    global: (props) => ({
       'html': {
         minWidth: '320px',
       },
@@ -20,9 +21,9 @@ export const AppTheme = extendTheme({
         lineHeight: 'var(--chakra-lineHeights-7)',
       },
       '::selection': {
-        background: 'yellow.400',
+        background: mode('yellow.400', 'yellow.500')(props),
       },
-    },
+    }),
   },
   components: {
     Container: {
