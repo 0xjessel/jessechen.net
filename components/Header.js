3efecf71c4adae59d69ce25546ed593ef1ef00fe
Jesse Chen <jessechen@fb.com>
2021-05-29 00:24:00 -1000

    Clean up header UI
    - Added more padding on links
    - Disabled link underline
    - Better responsive design

--- components/Header.js
+++ components/Header.js
@@ -1,38 +1,47 @@
 import NLink from 'next/link'
-import { Center, Flex, HStack, Link, Spacer, useColorMode } from "@chakra-ui/react"
+import { Center, Flex, HStack, Link, Spacer, useColorMode, useColorModeValue } from "@chakra-ui/react"
 import { FiSun } from 'react-icons/fi'
 
 export default function Header() {
   const { colorMode, toggleColorMode } = useColorMode();
 
+  const links = [
+    ['Projects', '/projects'],
+    ['Articles', '/articles'],
+    ['About', '/about'],
+  ]
+
   return (
-    <Flex as="nav" my={[0, 8]} mb={[8, null]}>
-      <HStack spacing={[4, 8]}>
+    <Flex as="nav" my={[0, 8]} mb={[4, 8]}>
+      <HStack spacing={[1, 4]}>
         <NLink href="/">
           <Link
             fontSize={['sm', 'md']}
+            mr={1}
             border="1px"
-            borderColor={colorMode === 'light' ? 'gray.800' : 'gray.50'}>
-            <Center fontSize={['md', 'lg']} px="2" py="1">
-              𝚓𝚎𝚜𝚜𝚎𝚕 ⟠
+            borderColor={useColorModeValue('gray.800', 'gray.50')}>
+            <Center 
+              fontSize={['md', 'lg']} 
+              whiteSpace="nowrap"
+              py={1}
+              px={2}>
+              𝚓𝚎𝚜𝚜𝚎𝚕 ⟠        
             </Center>
           </Link>
         </NLink>
-        <NLink href="/projects">
-          <Link fontSize={['sm', 'md']}>
-            Projects
-          </Link>
-        </NLink>
-        <NLink href="/articles">
-          <Link fontSize={['sm', 'md']}>
-            Articles
-          </Link>
-        </NLink>
-        <NLink href="/about">
-          <Link fontSize={['sm', 'md']}>
-            About
-          </Link>
-        </NLink>
+        {links.map(link => 
+          <NLink href={link[1]}>
+            <Link 
+              fontSize={['sm', 'md']}
+              borderRadius="base"
+              p={2}
+              _hover={{ 
+                'text-decoration': 'none',
+              }}>
+              {link[0]}
+            </Link>
+          </NLink>
+        )}
       </HStack>
       <Spacer />
       <HStack spacing={[2, 8]}>
