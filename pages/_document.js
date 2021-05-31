22b1f256fa27df3db0ca7a195cd2be01c66a63ca
Jesse Chen <jessechen@fb.com>
2021-05-30 18:44:01 -1000

    Integrate umami analytics

---
+++ pages/_document.js
@@ -0,0 +1,25 @@
+
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <script async defer data-website-id="6af95a0c-0756-43f8-8a73-fb9eefe3bffb" src="http://www.umami.jessechen.net:3000/umami.js"></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
