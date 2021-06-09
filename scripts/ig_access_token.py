529d37dad6351d3b9ea915fb29bc7029a6256a57
Jesse Chen <jessechen@fb.com>
2021-06-08 23:30:59 -1000

    Python script to renew long-lived IG access token
    Will have a cron job to run this every 30 days or so to renew the IG
    access token for fetching photos

---
+++ scripts/ig_access_token.py
@@ -0,0 +1,15 @@
+from urllib.request import urlopen
+from urllib.error import URLError, HTTPError
+
+IG_ACCESS_TOKEN = 'IGQVJYX3BRYS0wWGZAXbDNXaDlWbVJzeElWNU5Mdm9DZAkRyY1RGbW9NQlBBWXJVZAXhOTFFDRkFRMVB0LUF3ZAXc1QjR4b1ZAqaFNSeE92bnEwLXhUUXZAST3VRbFZAObEJHdHhBWFVDMW5Ub19zNHdCaExTZAgZDZD'
+
+try:
+  result = urlopen('https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token={access_token}'.format(access_token=IG_ACCESS_TOKEN)).read()
+except HTTPError as e:
+  print('Error code: ', e.code)
+except URLError as e:
+  print('Reason: ', e.reason)
+else:
+  print('✓ IG access token renewed')
+  print('\n === RESPONSE === \n')
+  print(result)
