f4fadad138e9ebceb770a55c4b31be35555fabe1
Jesse Chen <jessechen@fb.com>
2021-06-21 00:26:11 -1000

    Add console messages to generate-sitemap.mjs

--- scripts/generate-sitemap.mjs
+++ scripts/generate-sitemap.mjs
@@ -1,33 +1,42 @@
 import fs from 'fs'
 import globby from 'globby'
 
-// Ignore Next.js specific files (e.g., _app.js) and API routes.
-const pages = await globby([
-  'pages/**/*{.js,.mdx}',
-  '!pages/posts/[...*.js',
-  'posts/**/*{.js,.mdx}',
-  '!pages/_*.js',
-  '!pages/api',
-])
+try {
+  console.info('Generating sitemap...')
 
-const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${pages.map((page) => {
-      const path = page
-        .replace('pages', '')
-        .replace('posts', '/posts')
-        .replace('.js', '')
-        .replace('.mdx', '')
-        .replace('//', '/')
-      const route = path === '/index' ? '' : path
+  // Ignore Next.js specific files (e.g., _app.js) and API routes.
+  const pages = await globby([
+    'pages/**/*{.js,.mdx}',
+    '!pages/posts/[...*.js',
+    'posts/**/*{.js,.mdx}',
+    '!pages/_*.js',
+    '!pages/api',
+  ])
 
-      return `
-        <url>
-          <loc>${`https://jessechen.net${route}`}</loc>
-        </url>
-      `
-    }).join('')}
-  </urlset>
-`
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${pages.map((page) => {
+        const path = page
+          .replace('pages', '')
+          .replace('posts', '/posts')
+          .replace('.js', '')
+          .replace('.mdx', '')
+          .replace('//', '/')
+        const route = path === '/index' ? '' : path
 
-fs.writeFileSync('public/sitemap.xml', sitemap)
+        return `
+          <url>
+            <loc>${`https://jessechen.net${route}`}</loc>
+          </url>
+        `
+      }).join('')}
+    </urlset>
+  `
+
+  fs.writeFileSync('public/sitemap.xml', sitemap)
+
+  console.info('Sitemap generated successfully')
+} catch (e) {
+  console.error('Failed to generate sitemap')
+  console.error(e)
+}
