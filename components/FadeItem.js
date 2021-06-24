fd6b1c47d6271b7836698ffd22b5ecace6595c0b
Jesse Chen <jessechen@fb.com>
2021-06-23 22:17:26 -1000

    Disable animations on phones

--- components/FadeItem.js
+++ components/FadeItem.js
@@ -2,7 +2,7 @@ import { useBreakpointValue } from "@chakra-ui/react"
 import { Fade } from "react-awesome-reveal"
 
 export default function FadeItem({ children, ...props }) {
-  const tooSmall = useBreakpointValue({ sm: true, md: false })
+  const tooSmall = useBreakpointValue({ base: true, sm: true, md: false })
 
   if (tooSmall) {
     return children
